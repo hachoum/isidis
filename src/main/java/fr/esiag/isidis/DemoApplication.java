9c19d960c9de1d389ab379c68654931de5858466
il-isidis <hachmi.chbani@gmail.com>
2015-12-04 05:20:17 +0100

    first commit

---
+++ src/main/java/fr/esiag/isidis/DemoApplication.java
@@ -0,0 +1,12 @@
+package fr.esiag.isidis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
